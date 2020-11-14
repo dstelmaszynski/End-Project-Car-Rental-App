44770da5269a509163f0a14ad86a9e4b4e19a4b5
Dominik <stelmaszynski.dominik@gmail.com>
2020-11-14 14:34:53 +0100

    init0

---
+++ src/test/java/car_rent/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package car_rent.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
