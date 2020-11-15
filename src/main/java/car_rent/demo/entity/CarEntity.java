90df91ed24da8f325ebefe157bb86a4fd0406390
Dominik <stelmaszynski.dominik@gmail.com>
2020-11-15 13:31:26 +0100

    extending CarEntity

--- src/main/java/car_rent/demo/entity/CarEntity.java
+++ src/main/java/car_rent/demo/entity/CarEntity.java
@@ -12,6 +12,52 @@ public class CarEntity {
     @NotNull
     private Integer id;
     private String carBrand;
+    private String carModel;
+    private String carType;
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getCarProductionYear() {
+        return carProductionYear;
+    }
+
+    public void setCarProductionYear(String carProductionYear) {
+        this.carProductionYear = carProductionYear;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public Integer getCarMileage() {
+        return carMileage;
+    }
+
+    public void setCarMileage(Integer carMileage) {
+        this.carMileage = carMileage;
+    }
+
+    private String carProductionYear;
+    private String carColor;
+    private Integer carMileage;
     private String status;
     private Integer perDayCost;
 
