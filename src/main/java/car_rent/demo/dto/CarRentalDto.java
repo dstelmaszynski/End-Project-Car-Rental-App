dfa660834afddabe2b2798886e19def162c5195d
Dominik <stelmaszynski.dominik@gmail.com>
2020-11-15 11:30:59 +0100

    creating CarRentalDto

--- src/main/java/car_rent/demo/dto/CarRentalDto.java
+++ src/main/java/car_rent/demo/dto/CarRentalDto.java
@@ -1,4 +1,32 @@
 package car_rent.demo.dto;
 
+//to co przesyłamy między entity a uzytkownikiem
 public class CarRentalDto {
+    private String carBrand;
+    private String status;
+    private Integer perDayCost;
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getPerDayCost() {
+        return perDayCost;
+    }
+
+    public void setPerDayCost(Integer perDayCost) {
+        this.perDayCost = perDayCost;
+    }
 }
