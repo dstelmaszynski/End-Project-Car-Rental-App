a0090ef11f4fd2ec3019d6a8fdc16693f4f6a8d7
Michal <mike3129@gmail.com>
2020-11-22 13:59:17 +0100

    car list on reservation page

--- src/main/java/car_rent/demo/controller/ReservationController.java
+++ src/main/java/car_rent/demo/controller/ReservationController.java
@@ -1,15 +1,14 @@
 package car_rent.demo.controller;
 
 
+import car_rent.demo.dto.CarDto;
 import car_rent.demo.dto.ReservationDto;
+import car_rent.demo.service.CarService;
 import car_rent.demo.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +16,8 @@ import java.util.List;
 public class ReservationController {
     @Autowired
     private ReservationService reservationService;
+    @Autowired
+    private CarService carService;
 
     @RequestMapping(value = "/reservation", method = RequestMethod.GET)
     public String reservationList(Model model) {
@@ -31,9 +32,14 @@ public class ReservationController {
        //zdefiniowanie tych pól, setery na pobranie z http
        reservation.setReservationDateStart(startDate);
        reservation.setReservationDateEnd(endDate);
+        List<CarDto> dtos = carService.getAll();
        model.addAttribute("reservation", reservation);
+       model.addAttribute("carList", dtos);
+
         return "reservationCreate";
 }
+
+
     @RequestMapping(value = "/reservation/add", method = RequestMethod.POST)
     public String reservationCreate (@ModelAttribute("reservation") ReservationDto reservationDto) {
 
